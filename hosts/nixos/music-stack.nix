458d5b4147eb11e1a83f7fbd38a280bb26040294
vev <vev@brick.gay>
2026-06-06 15:15:54 -0700

    slskd: upload 512 KiB/s, download 10240 KiB/s

--- hosts/nixos/music-stack.nix
+++ hosts/nixos/music-stack.nix
@@ -203,7 +203,8 @@ in {
         incomplete = "/mnt/melody/media/Importing/.incomplete";
       };
       web.port = 5030;
-      global.upload.speed_limit = 256;
+      global.upload.speed_limit = 512;
+      global.download.speed_limit = 10240;
       flags.no_share_scan = false;
       # Share music back to the network (optional — set to false to leech only)
       shares.directories = ["/mnt/melody/media/Music"];
