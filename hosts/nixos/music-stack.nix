d45de1c20cf14946f580d4c445d74a74910870e0
vev <vev@brick.gay>
2026-06-08 03:16:48 -0700

    add jf.brick.gay reverse proxy to Jellyfin

--- hosts/nixos/music-stack.nix
+++ hosts/nixos/music-stack.nix
@@ -361,6 +361,22 @@ in {
           alias = "/mnt/melody/media/drives/drives.json";
         };
       };
+      "jf.brick.gay" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://192.168.7.101:8096";
+          extraConfig = ''
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          '';
+        };
+      };
     };
   };
 
