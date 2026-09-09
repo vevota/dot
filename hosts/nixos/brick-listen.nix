24b65da68422c1083d7ca901eb179c290841b68c
vev <vev@brick.gay>
2026-09-09 01:59:06 -0700

    fix nginx duplicate proxy directives for listen.brick.gay

--- hosts/nixos/brick-listen.nix
+++ hosts/nixos/brick-listen.nix
@@ -42,16 +42,9 @@ in
       proxyPass = "http://127.0.0.1:3070";
       proxyWebsockets = true;
       extraConfig = ''
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
         proxy_read_timeout 3600s;
         proxy_send_timeout 3600s;
-        client_max_body_size 4m;
       '';
     };
   };
